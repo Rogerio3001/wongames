11caeeb50c285c8ad25f7c9ec6bcbc0cac625ea1
Rogerio3001 <Rogeriovinicios3001@gmail.com>
2020-11-29 10:17:23 -0300

    chore: add menu component and icons

---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,27 @@
+import { ShoppingCart as ShoppingCartIcon } from '@styled-icons/material-outlined'
+import { Search as SearchIcon } from '@styled-icons/material-outlined'
+import { Menu2 as MenuIcon } from '@styled-icons/remix-fill'
+
+import * as S from './styles'
+import Logo from 'components/Logo'
+
+const Menu = () => (
+  <S.Wrapper>
+    <S.IconWrapper>
+      <MenuIcon />
+    </S.IconWrapper>
+    <S.LogoWrapper>
+      <Logo hideOnMobile />
+    </S.LogoWrapper>
+    <S.MenuGroup>
+      <S.IconWrapper>
+        <SearchIcon />
+      </S.IconWrapper>
+      <S.IconWrapper>
+        <ShoppingCartIcon />
+      </S.IconWrapper>
+    </S.MenuGroup>
+  </S.Wrapper>
+)
+
+export default Menu
