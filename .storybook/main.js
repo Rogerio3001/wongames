58ea43370336b4a9549d7be1e8b9d7e5b9305d6c
rogerio3001 <rogeriovinicios3001@gmail.com>
2021-09-17 20:12:24 -0300

    add: dark theme from storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,10 @@
 module.exports = {
   stories: ['../src/components/**/stories.tsx'],
-  addons: ['@storybook/addon-essentials', 'storybook-addon-next-router'],
+  addons: [
+    '@storybook/addon-essentials',
+    'storybook-addon-next-router',
+    'storybook-dark-mode'
+  ],
   babel: async (options) => ({
     ...options,
     plugins: [
