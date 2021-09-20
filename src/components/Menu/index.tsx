b9f88c34ff22287d681be984f5bfd8d504265c07
rogerio3001 <rogeriovinicios3001@gmail.com>
2021-09-20 20:09:32 -0300

    add: Menu component (Mobile and Desktop)

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,29 +1,80 @@
+import { useState } from 'react'
 import * as S from './styles'
 import { Menu2 as MenuIcon } from '@styled-icons/remix-fill/Menu2'
 import { ShoppingCart as ShoppingCartIcon } from '@styled-icons/material-outlined/ShoppingCart'
 import { Search as SearchIcon } from '@styled-icons/material-outlined/Search'
+import { Close as CloseIcon } from '@styled-icons/material-outlined/Close'
 
 import Logo from 'components/Logo'
+import Button from 'components/Button'
+import MediaMatch from 'components/MediaMatch'
 
-const Menu = () => (
-  <S.Wrapper>
-    <S.IconWrapper aria-label="Open Menu">
-      <MenuIcon />
-    </S.IconWrapper>
-    <S.LogoWrapper>
-      <Logo hideOnMobile />
-    </S.LogoWrapper>
+export type MenuProps = {
+  username?: string
+}
 
-    <S.MenuGroup>
-      <S.IconWrapper>
-        <SearchIcon aria-label="Search" />
-      </S.IconWrapper>
+const Menu = ({ username }: MenuProps) => {
+  const [isOpen, setIsOpen] = useState(false)
 
-      <S.IconWrapper>
-        <ShoppingCartIcon aria-label="Open Shopping Cart" />
-      </S.IconWrapper>
-    </S.MenuGroup>
-  </S.Wrapper>
-)
+  return (
+    <S.Wrapper>
+      <MediaMatch lessThan="medium">
+        <S.IconWrapper onClick={() => setIsOpen(true)}>
+          <MenuIcon aria-label="Open Menu" />
+        </S.IconWrapper>
+      </MediaMatch>
+
+      <S.LogoWrapper>
+        <Logo hideOnMobile />
+      </S.LogoWrapper>
+
+      <MediaMatch greaterThan="medium">
+        <S.MenuNav>
+          <S.MenuLink href="#">Home</S.MenuLink>
+          <S.MenuLink href="#">Explore</S.MenuLink>
+        </S.MenuNav>
+      </MediaMatch>
+
+      <S.MenuGroup>
+        <S.IconWrapper>
+          <SearchIcon aria-label="Search" />
+        </S.IconWrapper>
+
+        <S.IconWrapper>
+          <ShoppingCartIcon aria-label="Open Shopping Cart" />
+        </S.IconWrapper>
+        {!username && (
+          <MediaMatch greaterThan="medium">
+            <Button>Sign In</Button>
+          </MediaMatch>
+        )}
+      </S.MenuGroup>
+      <S.MenuFull aria-hidden={!isOpen} isOpen={isOpen}>
+        <CloseIcon aria-label="Close menu" onClick={() => setIsOpen(false)} />
+        <S.MenuNav>
+          <S.MenuLink href="#">Home</S.MenuLink>
+          <S.MenuLink href="#">Explore</S.MenuLink>
+          {!!username && (
+            <>
+              <S.MenuLink href="#">My account</S.MenuLink>
+              <S.MenuLink href="#">WishList</S.MenuLink>
+            </>
+          )}
+        </S.MenuNav>
+        {!username && (
+          <S.RegisterBox>
+            <Button fullWidth size="large">
+              Log In Now
+            </Button>
+            <span>or</span>
+            <S.CreateAccount href="#" title="signin">
+              Sign Up
+            </S.CreateAccount>
+          </S.RegisterBox>
+        )}
+      </S.MenuFull>
+    </S.Wrapper>
+  )
+}
 
 export default Menu
--- src/components/Menu/test.tsx
+++ src/components/Menu/test.tsx
@@ -1,4 +1,4 @@
-import { screen } from '@testing-library/react'
+import { fireEvent, screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
 
 import Menu from '.'
@@ -11,5 +11,44 @@ describe('<Menu />', () => {
     expect(screen.getByLabelText(/search/i)).toBeInTheDocument()
     expect(screen.getByLabelText(/open shopping cart/i)).toBeInTheDocument()
     expect(screen.getByRole('img', { name: /won games/i })).toBeInTheDocument()
+  })
+
+  it('should handle the open/close mobile menu', () => {
+    renderWithTheme(<Menu />)
+
+    //selecionar MenuFull
+    const fullMenuElement = screen.getByRole('navigation', { hidden: true })
+
+    //Veficiar se o menu esta invisivel
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
+    expect(fullMenuElement).toHaveStyle({ opacity: 0 })
+
+    //Clicar e verificar se abriu
+    fireEvent.click(screen.getByLabelText(/open menu/i))
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('false')
+    expect(fullMenuElement).toHaveStyle({ opacity: 1 })
+
+    //Clicar em fechar o menu e verificar se fechou
+    fireEvent.click(screen.getByLabelText(/close menu/i))
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
+    expect(fullMenuElement).toHaveStyle({ opacity: 0 })
+  })
+
+  it('should show register box when logged out', () => {
+    renderWithTheme(<Menu />)
+
+    expect(screen.getByText(/log in now/i)).toBeInTheDocument()
+    expect(screen.getByText(/sign up/i)).toBeInTheDocument()
+    expect(screen.queryByText(/wishlist/i)).not.toBeInTheDocument()
+    expect(screen.queryByText(/my account/i)).not.toBeInTheDocument()
+  })
+
+  it('should show wishlist and account when logged in', () => {
+    renderWithTheme(<Menu username="roger" />)
+
+    expect(screen.getByText(/wishlist/i)).toBeInTheDocument()
+    expect(screen.getByText(/my account/i)).toBeInTheDocument()
+    expect(screen.queryByText(/log in now/i)).not.toBeInTheDocument()
+    expect(screen.queryByText(/sign up/i)).not.toBeInTheDocument()
   })
 })
