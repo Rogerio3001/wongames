0b0704b63eec2c4423bc358b9b10a43367c8d697
rogerio3001 <rogeriovinicios3001@gmail.com>
2022-07-21 08:50:33 -0300

    add: home page, home template screen

--- src/components/Menu/styles.ts
+++ src/components/Menu/styles.ts
@@ -1,4 +1,5 @@
 import styled, { css } from 'styled-components'
+
 import media from 'styled-media-query'
 
 export const Wrapper = styled.menu`
@@ -18,42 +19,43 @@ export const LogoWrapper = styled.div`
   `}
 `
 
+export const IconWrapper = styled.div`
+  ${({ theme }) => css`
+    color: ${theme.colors.white};
+    cursor: pointer;
+    width: 2.4rem;
+    height: 2.4rem;
+  `}
+`
+
 export const MenuGroup = styled.div`
   ${({ theme }) => css`
     display: flex;
     flex-grow: 1;
     justify-content: flex-end;
     align-items: center;
-
     > div {
       margin-left: ${theme.spacings.xsmall};
     }
   `}
 `
 
-export const IconWrapper = styled.div`
-  ${({ theme }) => css`
-    color: ${theme.colors.white};
-    width: 2.4rem;
-    height: 2.4rem;
-    cursor: pointer;
-  `}
-`
-
 export const MenuNav = styled.div`
   ${({ theme }) => css`
-    margin-left: ${theme.spacings.small};
+    ${media.greaterThan('medium')`
+			margin-left: ${theme.spacings.small};
+		`}
   `}
 `
 
 export const MenuLink = styled.a`
   ${({ theme }) => css`
     position: relative;
+    color: ${theme.colors.white};
     font-size: ${theme.font.sizes.medium};
     margin: 0.3rem ${theme.spacings.small} 0;
     text-decoration: none;
     text-align: center;
-    color: ${theme.colors.white};
     &:hover {
       &::after {
         content: '';
@@ -80,28 +82,24 @@ export const MenuLink = styled.a`
 type MenuFullProps = {
   isOpen: boolean
 }
+
 export const MenuFull = styled.nav<MenuFullProps>`
   ${({ theme, isOpen }) => css`
     display: flex;
     flex-direction: column;
     justify-content: space-between;
-
-    //Logica de open close do menu usando opacidade no lugar do display block
-    transition: opacity 0.3s ease-in-out;
-    opacity: ${isOpen ? 1 : 0};
-    pointer-events: ${isOpen ? 'all' : 'none'};
-
-    //Inserindo o menu na frente da tela atual
     background: ${theme.colors.white};
-    position: absolute;
+    position: fixed;
+    z-index: ${theme.layers.menu};
     top: 0;
     bottom: 0;
     left: 0;
     right: 0;
     height: 100vh;
     overflow: hidden;
-
-    //Colocando o icone no canto direito absolutado
+    transition: opacity 0.3s ease-in-out;
+    opacity: ${isOpen ? 1 : 0};
+    pointer-events: ${isOpen ? 'all' : 'none'};
     > svg {
       position: absolute;
       top: 0;
@@ -111,7 +109,6 @@ export const MenuFull = styled.nav<MenuFullProps>`
       width: 2.4rem;
       height: 2.4rem;
     }
-
     ${MenuNav} {
       display: flex;
       align-items: center;
@@ -119,7 +116,6 @@ export const MenuFull = styled.nav<MenuFullProps>`
       flex: 1;
       flex-direction: column;
     }
-
     ${MenuLink} {
       color: ${theme.colors.black};
       font-weight: ${theme.font.bold};
@@ -128,13 +124,13 @@ export const MenuFull = styled.nav<MenuFullProps>`
       transform: ${isOpen ? 'translateY(0)' : 'translateY(3rem)'};
       transition: transform 0.3s ease-in-out;
     }
-
     ${RegisterBox} {
       transform: ${isOpen ? 'translateY(0)' : 'translateY(3rem)'};
       transition: transform 0.3s ease-in-out;
     }
   `}
 `
+
 export const RegisterBox = styled.div`
   ${({ theme }) => css`
     display: flex;
@@ -148,6 +144,7 @@ export const RegisterBox = styled.div`
     }
   `}
 `
+
 export const CreateAccount = styled.a`
   ${({ theme }) => css`
     text-decoration: none;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,28 @@
-import Main from 'components/Main'
+// You should use getServerSideProps when:
+import Home, { HomeTemplateProps } from 'templates/Home'
 
-export default function Home() {
-  return <Main />
+import bannersMock from 'components/BannerSlider/mock'
+import gamesMock from 'components/GameCardSlider/mock'
+import highlightMock from 'components/Highlight/mock'
+
+export default function Index(props: HomeTemplateProps) {
+  return <Home {...props} />
+}
+
+export function getServerSideProps() {
+  //  const { data } = await  // your fetch function here
+
+  return {
+    props: {
+      banners: bannersMock,
+      newGames: gamesMock,
+      mostPopularHighlight: highlightMock,
+      mostPopularGames: gamesMock,
+      upcommingGames: gamesMock,
+      upcommingHighligth: highlightMock,
+      upcommingMoreGames: gamesMock,
+      freeGames: gamesMock,
+      freeHighligth: highlightMock
+    }
+  }
 }
